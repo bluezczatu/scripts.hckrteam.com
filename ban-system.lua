246eb0be8f38b43b622751139fd2c217a2a9a435
Jakub <74353981+bluezczatu@users.noreply.github.com>
2024-01-15 13:53:07 +0100

    Update ban-system.lua

--- ban-system.lua
+++ ban-system.lua
@@ -8,7 +8,7 @@ local Players = game:GetService("Players")
 
 function check(player)
 	local HttpService = game:GetService("HttpService")
-	local url = "http://api-pegasus.hckrteam.com/v2/live"
+	local url = "http://api.pegasus.bot/v2/live"
 	
 	local requestHeaders = {
 		['Authorization'] = authkey
